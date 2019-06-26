3365d66f1515d6bee890c3df7ddd5695a3d05a5c
Sigmundur Mørkøre <sigmundurmorkore@gmail.com>
2019-06-26 21:29:37 +0100

    Created a project component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Img from 'gatsby-image'
 import styled, { css } from 'styled-components'
 
 import SEO from '../components/seo'
+import Project from '../components/project'
 import header from '../images/header.jpg'
 
 const Header = styled.header`
@@ -63,72 +64,6 @@ const SectionTitle = styled.h2`
   }
 `
 
-const Project = styled.div`
-  display: grid;
-  grid-template:
-    'title'
-    'image'
-    'content'
-    'socials';
-  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);
-  padding: 20px;
-  justify-items: center;
-
-  transition: 0.3s ease-in-out;
-
-  :hover {
-    transform: scale(1.02);
-    transition: 0.3s ease-in-out;
-  }
-
-  h3 {
-    grid-area: title;
-    text-align: center;
-    margin-top: 0;
-    border-bottom: 1px solid #011627;
-    width: 240px;
-    height: 30px;
-
-    a {
-      text-decoration: none;
-    }
-  }
-
-  .gatsby-image-wrapper {
-    grid-area: image;
-    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);
-  }
-
-  h4 {
-    grid-area: content;
-  }
-
-  ul {
-    display: grid;
-    grid-area: socials;
-    grid-template-columns: repeat(auto-fit, minmax(0px, 1fr));
-    grid-gap: 0px;
-    justify-content: center;
-    width: 75%;
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-
-    h5 {
-      display: inline;
-      padding-right: 5px;
-    }
-
-    li {
-      justify-self: center;
-
-      i {
-        vertical-align: middle;
-      }
-    }
-  }
-`
-
 const ContactBox = styled.a`
   text-decoration: none;
   box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);
@@ -155,158 +90,150 @@ const ContactBox = styled.a`
   }
 `
 
-const IndexPage = ({ data }) => {
-  console.log(data)
-  return (
-    <>
-      <SEO title="Sigmundur Mørkøre's Portfolio" />
-      <Header>
-        <h1
-          css={`
+const IndexPage = ({ data }) => (
+  <>
+    <SEO title="Sigmundur Mørkøre's Portfolio" />
+    <Header>
+      <h1
+        css={`
             display: inline-block
             font-family: Brawler;
             font-size: 64px;
             padding-top: 15%;
             margin-right: 50px;
           `}
-        >
-          Hello, I am{' '}
-          <b
-            css={`
-              font-family: 'Bree Serif';
-            `}
-          >
-            Sigmundur Mørkøre
-          </b>
-          <p
-            css={`
-              margin: 0;
-              padding-top: 10px;
-              margin-right: -50px;
-              font-size: 36px;
-              text-align: right;
-            `}
-          >
-            I make and design great looking websites.
-          </p>
-        </h1>
-      </Header>
-      <main
-        css={css`
-          margin: 50px 5vw;
-        `}
       >
-        <section
-          css={css`
-            display: grid;
-            grid-column-gap: 50px;
-            grid-template:
-              '. about about aside'
-              '. content content aside';
-            margin: 0 10vw;
+        Hello, I am{' '}
+        <b
+          css={`
+            font-family: 'Bree Serif';
           `}
         >
-          <h2
+          Sigmundur Mørkøre
+        </b>
+        <p
+          css={`
+            margin: 0;
+            padding-top: 10px;
+            margin-right: -50px;
+            font-size: 36px;
+            text-align: right;
+          `}
+        >
+          I make and design great looking websites.
+        </p>
+      </h1>
+    </Header>
+    <main
+      css={css`
+        margin: 50px 5vw;
+      `}
+    >
+      <section
+        css={css`
+          display: grid;
+          grid-column-gap: 50px;
+          grid-template:
+            '. about about aside'
+            '. content content aside';
+          margin: 0 10vw;
+        `}
+      >
+        <h2
+          css={css`
+            grid-area: about;
+            margin-bottom: 10px;
+            display: flex;
+            align-items: flex-end;
+          `}
+        >
+          About me
+        </h2>
+        <h3
+          css={css`
+            grid-area: content;
+            margin-top: 10px;
+          `}
+        >
+          Hi, my name is <b>Sigmundur Mørkøre</b>. I’m born in a country with 18
+          sheep filled islands, called the <b>Faroe Islands</b>. I went to
+          Higher Technical Examination (HTX) school, where I gained a passion
+          for <b>web development</b>. I made many websites while in HTX, and in
+          the last year, I developed{' '}
+          <a href="https://ferdaaetlan.fo">www.ferdaaetlan.fo</a>. After
+          graduating I went to, and am in, <b>Edge Hill University</b> studying
+          a bachelor in <b>Web Design & Development.</b>
+        </h3>
+        <aside
+          css={css`
+            grid-area: aside;
+            width: 250px;
+            height: 300px;
+            box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);
+            margin: 0 auto;
+            margin-top: 50px;
+
+            transition: 0.3s ease-in-out;
+
+            :hover {
+              transform: scale(1.02);
+              transition: 0.3s ease-in-out;
+            }
+          `}
+        >
+          <Img
+            alt="Profile picture"
+            fixed={data.profileImg.childImageSharp.fixed}
+            style={{ borderRadius: '50%', display: 'block' }}
             css={css`
-              grid-area: about;
-              margin-bottom: 10px;
-              display: flex;
-              align-items: flex-end;
+              margin-top: -50px;
+              margin-left: auto;
+              margin-right: auto;
+              box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);
             `}
-          >
-            About me
-          </h2>
+          />
           <h3
             css={css`
-              grid-area: content;
+              text-align: center;
+              margin-bottom: 0;
               margin-top: 10px;
             `}
           >
-            Hi, my name is <b>Sigmundur Mørkøre</b>. I’m born in a country with
-            18 sheep filled islands, called the <b>Faroe Islands</b>. I went to
-            Higher Technical Examination (HTX) school, where I gained a passion
-            for <b>web development</b>. I made many websites while in HTX, and
-            in the last year, I developed{' '}
-            <a href="https://ferdaaetlan.fo">www.ferdaaetlan.fo</a>. After
-            graduating I went to, and am in, <b>Edge Hill University</b>{' '}
-            studying a bachelor in <b>Web Design & Development.</b>
+            <b>Contact me</b>
           </h3>
-          <aside
+          <dl
             css={css`
-              grid-area: aside;
-              width: 250px;
-              height: 300px;
-              box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);
-              margin: 0 auto;
-              margin-top: 50px;
-
-              transition: 0.3s ease-in-out;
-
-              :hover {
-                transform: scale(1.02);
-                transition: 0.3s ease-in-out;
-              }
+              margin: 0 22px;
             `}
           >
-            <Img
-              alt="Profile picture"
-              fixed={data.profileImg.childImageSharp.fixed}
-              style={{ 'border-radius': '50%', display: 'block' }}
-              css={css`
-                margin-top: -50px;
-                margin-left: auto;
-                margin-right: auto;
-                box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);
-              `}
-            />
-            <h3
-              css={css`
-                text-align: center;
-                margin-bottom: 0;
-                margin-top: 10px;
-              `}
-            >
-              <b>Contact me</b>
-            </h3>
-            <dl
-              css={css`
-                margin: 0 22px;
-              `}
-            >
-              <dt>Email</dt>
-              <dd>sm@sigmundur.dev</dd>
-              <dt>Twitter</dt>
-              <dd>@SigmundurM</dd>
-              <dt>GitHub</dt>
-              <dd>@SigmundurMorkore</dd>
-              <dt>Blog</dt>
-              <dd>sigmundur.dev/blog</dd>
-            </dl>
-          </aside>
-        </section>
-        <section>
-          <SectionTitle>
-            <span>Featured Projects</span>
-          </SectionTitle>
-          <div
-            css={css`
-              display: grid;
-              grid-template-columns: repeat(auto-fit, minmax(350px, 500px));
-              grid-gap: 50px;
-              justify-content: center;
-            `}
-          >
-            <Project>
-              <h3>
-                <a href="https://ferdaaetlan.fo">Ferdaaetlan.fo</a>
-              </h3>
-              <a href="https://ferdaaetlan.fo">
-                <Img
-                  alt="Screenshot of the ferdaaetlan.fo page"
-                  fixed={data.ferdaaetlanImg.childImageSharp.fixed}
-                />
-              </a>
-              <h4>
+            <dt>Email</dt>
+            <dd>sm@sigmundur.dev</dd>
+            <dt>Twitter</dt>
+            <dd>@SigmundurM</dd>
+            <dt>GitHub</dt>
+            <dd>@SigmundurMorkore</dd>
+            <dt>Blog</dt>
+            <dd>sigmundur.dev/blog</dd>
+          </dl>
+        </aside>
+      </section>
+      <section>
+        <SectionTitle>
+          <span>Featured Projects</span>
+        </SectionTitle>
+        <div
+          css={css`
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(350px, 500px));
+            grid-gap: 50px;
+            justify-content: center;
+          `}
+        >
+          <Project
+            title="Ferdaaetlan.fo"
+            link="https://ferdaaetlan.fo"
+            content={
+              <>
                 I developed{' '}
                 <b>
                   <a href="https://ferdaaetlan.fo">Ferðaætlan</a>
@@ -315,85 +242,81 @@ const IndexPage = ({ data }) => {
                 the user to see which SSL bus routes they need to take, to get
                 to their destination. The website is developed using{' '}
                 <b>React and Firebase.</b>
-              </h4>
-              <ul
-                css={css`
-                  min-height: 20.75px;
-                `}
-              >
-                <li>
-                  <h5>View on GitHub</h5>
-                  <i class="fab fa-github" />
-                </li>
-                <li>
-                  <h5>Read more</h5>
-                  <i class="fal fa-newspaper" />
-                </li>
-              </ul>
-            </Project>
-            <Project>
-              <h3>
-                <a href="https://hvar.fo">Hvar.fo</a>
-              </h3>
-              <a href="https://hvar.fo">
-                <Img
-                  alt="Screenshot of the hvar.fo page"
-                  fixed={data.hvarImg.childImageSharp.fixed}
-                  objectFit="cover"
-                />
-              </a>
-              <h4>
-                <b>
-                  <a href="https://hvar.fo">Hvar.fo</a>
-                </b>{' '}
+              </>
+            }
+            image={
+              <Img
+                alt="Screenshot of the ferdaaetlan.fo page"
+                fixed={data.ferdaaetlanImg.childImageSharp.fixed}
+              />
+            }
+            socials={[
+              {
+                label: 'View on GitHub',
+                icon: 'fab fa-github',
+              },
+              {
+                label: 'Read more',
+                icon: 'fal fa-newspaper',
+              },
+            ]}
+          />
+          <Project
+            title="Hvar.fo"
+            link="https://hvar.fo"
+            content={
+              <>
                 is a list over all “.fo” domains. All the websites are
                 categorized into categories, and the search field allows the
                 user to search for anything they want. The website is developed
                 using <b>React, Airtable, and Netlify (Functions).</b>
-              </h4>
-              <ul
-                css={css`
-                  min-height: 20.75px;
-                `}
-              >
-                <li>
-                  <h5>Read more</h5>
-                  <i class="fal fa-newspaper" />
-                </li>
-              </ul>
-            </Project>
-          </div>
-        </section>
-        <section>
-          <SectionTitle>
-            <span>Contact Me</span>
-          </SectionTitle>
-          <dl
-            css={css`
-              display: grid;
-              grid-template-columns: repeat(auto-fit, minmax(270px, 375px));
-              grid-gap: 50px;
-              justify-content: center;
-            `}
-          >
-            <ContactBox href="#">
-              <dt>Email</dt>
-              <dd>sm@sigmundur.dev</dd>
-            </ContactBox>
-            <ContactBox href="#">
-              <dt>Twitter</dt>
-              <dd>@SigmundurM</dd>
-            </ContactBox>
-            <ContactBox href="#">
-              <dt>GitHub</dt>
-              <dd>@SigmundurMorkore</dd>
-            </ContactBox>
-          </dl>
-        </section>
-      </main>
-    </>
-  )
-}
+              </>
+            }
+            image={
+              <Img
+                alt="Screenshot of the hvar.fo page"
+                fixed={data.hvarImg.childImageSharp.fixed}
+                objectFit="cover"
+              />
+            }
+            socials={[
+              {
+                label: 'Read more',
+                icon: 'fal fa-newspaper',
+              },
+            ]}
+          />
+        </div>
+      </section>
+      <section>
+        <SectionTitle>
+          <span>Contact Me</span>
+        </SectionTitle>
+        <dl
+          css={css`
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(270px, 375px));
+            grid-gap: 50px;
+            justify-content: center;
+          `}
+        >
+          <ContactBox href="#">
+            <dt>Email</dt>
+            <dd>sm@sigmundur.dev</dd>
+          </ContactBox>
+          <ContactBox href="#">
+            <dt>Twitter</dt>
+            <dd>@SigmundurM</dd>
+          </ContactBox>
+          <ContactBox href="#">
+            <dt>GitHub</dt>
+            <dd>@SigmundurMorkore</dd>
+          </ContactBox>
+        </dl>
+      </section>
+    </main>
+  </>
+)
 
 /*
       <footer
